0bae3f16dfdc37667b0549b8b5cff2626e8962c2
MahdiAghaali <mahdi.aghaali@yahoo.com>
2022-11-03 13:22:25 +0330

    Add routing to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
-import Calculator from './components/Calculator';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
+import CalcSection from './components/CalcSection';
 
 export default class App extends React.PureComponent {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<CalcSection />} />
+          <Route path="/quotes" element={<Quotes />} />
+        </Routes>
       </div>
     );
   }
