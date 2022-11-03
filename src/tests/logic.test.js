a26f17cadbf9f9e492ce12130f9e99dc585d6617
Odette Gleen <nkargleeny@gmail.com>
2022-11-03 13:52:47 +0100

    setup test for operation

--- src/tests/logic.test.js
+++ src/tests/logic.test.js
@@ -2,7 +2,12 @@ import operate from '../logic/operate';
 
 describe('operate(numberOne, numberTwo, operation)', () => {
   it('test', () => {
-    const test = operate('1', '1', '+');
-    expect(test).toBe('2');
+    expect(operate('1', '1', '+')).toBe('2');
+    expect(operate('1', '13', '+')).toBe('14');
+  });
+  
+  it ('test', () => {
+    const test = operate ('10', '5', 'x');
+    expect(test).toBe ('50');
   });
 });
